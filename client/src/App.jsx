cf077655c6b88faececd2df3ba473224839a14cf
Michael F. Alvarez <37319530+awwmicky@users.noreply.github.com>
2021-09-27 18:40:59 -0700

    minor file update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-
-import { FooterNavigation, HeaderOption } from './containers'
-import {
-	ContentPage, ErrorPage, HomePage, LoginPage, PostPage, RegisterPage, TopicPage 
-} from './pages'
 import { PrivateRoute, ProtectedRoute } from './routes'
+import { FooterNavigation, HeaderOption } from './containers'
+import { 
+	HomePage,
+	ContentPage,
+	PostPage, 
+	TopicPage,
+	LoginPage, 
+	RegisterPage,
+	ErrorPage 
+} from './pages'
 
 const App = () => {
 	return (
@@ -17,9 +22,9 @@ const App = () => {
 				<Route path="/blog/content" component={ ContentPage } /> {/* Content */}
 				<Route path="/admin/post" component={ PostPage } /> {/* Post */}
 				<Route path="/user/topic" component={ TopicPage } /> {/* Topic */}
-				<Route path="*" component={ ErrorPage } /> {/* Error Page */}
 				<ProtectedRoute path="/login" element={ LoginPage } /> {/* Login */}
 				<ProtectedRoute path="/register" component={ RegisterPage } /> {/* Register */}
+				<Route path="*" component={ ErrorPage } /> {/* Error Page */}
 			</Switch>
 
 			<FooterNavigation />
