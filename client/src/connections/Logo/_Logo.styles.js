1ff22f89aa5cb6f4bca5d8d6292f48ba7f831c17
awwmicky <michael_fred_alvarez@yahoo.com>
2021-09-29 03:01:31 -0700

    create active nav link

--- client/src/connections/Logo/_Logo.styles.js
+++ client/src/connections/Logo/_Logo.styles.js
@@ -8,7 +8,6 @@ import { Icon } from '../../components'
 
 export const LogoBox = chakra(Link, {
 	baseStyle: {
-		border: '1px',
 		display: 'flex',
 		alignItems: 'center',
 		justifyContent: 'center'
@@ -19,7 +18,7 @@ export const LogoIcon = chakra(Icon.LogoIcon, {
 	baseStyle: {
 		w: '2em',
 		h: '2em',
-		color: 'red.500'
+		color: 'teal.500'
 	}
 })
 
@@ -27,7 +26,7 @@ export const LogoImage = chakra(Image, {})
 
 export const LogoHeading = chakra(Heading, {
 	baseStyle: {
-		// fontSize: 'lg',
-		fontWeight: 'bold'
+		fontWeight: 'bold',
+		pl: 1.5
 	}
 })
---
+++ client/src/connections/NavLink/_NavLink.styles.js
@@ -0,0 +1,27 @@
+import {
+	chakra,
+	Center,
+	Text
+} from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+
+export const NavLinkBox = chakra(Link, {
+	baseStyle: {
+		border: '1px',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
+})
+
+export const LinkCenter = chakra(Center, {
+	baseStyle: {
+		flexFlow: 'column'
+	}
+})
+
+export const LinkText = chakra(Text, {
+	baseStyle: {
+		fontWeight: 'bold'
+	}
+})
